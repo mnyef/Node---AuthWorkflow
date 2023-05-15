ff704db49d4a6bf50eddd7e06d9d6b03b02c01d7
mnyef <vladgav004@gmail.com>
2023-05-15 20:07:38 +0300

    Verify Email Controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
 const { attachCookiesToResponse, createTokenUser } = require('../utils');
 const crypto = require('crypto');
+const { STATUS_CODES } = require('http');
 
 const register = async (req, res) => {
   const { email, name, password } = req.body;
@@ -32,6 +33,27 @@ const register = async (req, res) => {
     verificationToken: user.verificationToken,
   });
 };
+
+const verifyEmail = async (req, res) => {
+  const { verificationToken, email } = req.body;
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new CustomError.UnauthenticatedError('Verification Failed');
+  }
+
+  if (user.verificationToken !== verificationToken) {
+    throw new CustomError.UnauthenticatedError('Verification Failed');
+  }
+
+  (user.isVerified = true), (user.verified = Date.now());
+  user.verificationToken = '';
+
+  await user.save();
+
+  res.status(StatusCodes.OK).json({ msg: 'Email Verified' });
+};
+
 const login = async (req, res) => {
   const { email, password } = req.body;
 
@@ -68,4 +90,5 @@ module.exports = {
   register,
   login,
   logout,
+  verifyEmail,
 };
