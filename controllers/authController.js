41275ec332b030be062992c8239f11c06d393b08
mnyef <vladgav004@gmail.com>
2023-05-15 19:48:07 +0300

    Proper Verification Token

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
 const { attachCookiesToResponse, createTokenUser } = require('../utils');
+const crypto = require('crypto');
 
 const register = async (req, res) => {
   const { email, name, password } = req.body;
@@ -15,7 +16,7 @@ const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments({})) === 0;
   const role = isFirstAccount ? 'admin' : 'user';
 
-  const verificationToken = 'fake token';
+  const verificationToken = crypto.randomBytes(40).toString('hex');
 
   const user = await User.create({
     name,
