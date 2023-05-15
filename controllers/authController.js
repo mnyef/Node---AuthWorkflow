7ec60a0c263846941b0ae01fd4337306bc8abe11
mnyef <vladgav004@gmail.com>
2023-05-15 19:33:19 +0300

    Refactor (Register Controller)

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,10 +15,21 @@ const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments({})) === 0;
   const role = isFirstAccount ? 'admin' : 'user';
 
-  const user = await User.create({ name, email, password, role });
-  const tokenUser = createTokenUser(user);
-  attachCookiesToResponse({ res, user: tokenUser });
-  res.status(StatusCodes.CREATED).json({ user: tokenUser });
+  const verificationToken = 'fake token';
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+    verificationToken,
+  });
+
+  // Send verification token back only while testing in postman
+  res.status(StatusCodes.CREATED).json({
+    msg: 'Success! Please check your email to verify your account',
+    verificationToken: user.verificationToken,
+  });
 };
 const login = async (req, res) => {
   const { email, password } = req.body;
