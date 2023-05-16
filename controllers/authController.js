ca578e5819ad60787698cbd38526a8aec616ec8e
mnyef <vladgav004@gmail.com>
2023-05-16 13:47:04 +0300

    Send Email

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,12 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
-const { attachCookiesToResponse, createTokenUser } = require('../utils');
+const {
+  attachCookiesToResponse,
+  createTokenUser,
+  sendVerificationEmail,
+} = require('../utils');
 const crypto = require('crypto');
-const { STATUS_CODES } = require('http');
 
 const register = async (req, res) => {
   const { email, name, password } = req.body;
@@ -27,10 +30,17 @@ const register = async (req, res) => {
     verificationToken,
   });
 
+  const origin = 'http://localhost:3000';
+
+  await sendVerificationEmail({
+    name: user.name,
+    email: user.email,
+    verificationToken: user.verificationToken,
+    origin,
+  });
   // Send verification token back only while testing in postman
   res.status(StatusCodes.CREATED).json({
     msg: 'Success! Please check your email to verify your account',
-    verificationToken: user.verificationToken,
   });
 };
 
---
+++ utils/nodemailerConfig.js
@@ -0,0 +1,8 @@
+module.exports = {
+  host: 'smtp.ethereal.email',
+  port: 587,
+  auth: {
+    user: 'cody.haag@ethereal.email',
+    pass: '42nbz7pHdBd4fFQNmH',
+  },
+};
