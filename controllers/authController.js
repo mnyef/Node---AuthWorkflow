fdbffa25083c2ea02a8c8a3a4b68afd1886a6659
mnyef <vladgav004@gmail.com>
2023-05-16 23:00:51 +0300

    Forgot Password Controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -131,8 +131,29 @@ const logout = async (req, res) => {
 };
 
 const forgotPassword = async (req, res) => {
-  res.send('forgot password');
+  const { email } = req.body;
+  if (!email) {
+    throw new CustomError.BadRequestError('Please provide valid email');
+  }
+
+  const user = await User.findOne({ email });
+  if (user) {
+    const passwordToken = crypto.randomBytes(70).toString('hex');
+    // send email
+
+    const tenMinutes = 1000 * 60 * 10;
+    const passwordTokenExpirationDate = new Date(Date.now() + tenMinutes);
+
+    user.passwordToken = passwordToken;
+    user.passwordTokenExpirationDate = passwordTokenExpirationDate;
+    await user.save();
+  }
+
+  res
+    .status(StatusCodes.OK)
+    .json({ msg: 'Please check your email for reset password link' });
 };
+
 const resetPassword = async (req, res) => {
   res.send('reset password');
 };
