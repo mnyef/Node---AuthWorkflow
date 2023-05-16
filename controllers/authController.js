33a3a54e96f0289eb6de59f01ede2e48af5f2240
mnyef <vladgav004@gmail.com>
2023-05-16 21:51:47 +0300

    Send Multiple Cookies

--- controllers/authController.js
+++ controllers/authController.js
@@ -96,11 +96,11 @@ const login = async (req, res) => {
   const ip = req.ip;
   const userToken = { refreshToken, ip, userAgent, user: user._id };
 
-  const token = await Token.create(userToken);
+  await Token.create(userToken);
 
-  // attachCookiesToResponse({ res, user: tokenUser });
+  attachCookiesToResponse({ res, user: tokenUser, refreshToken });
 
-  res.status(StatusCodes.OK).json({ user: tokenUser, token });
+  res.status(StatusCodes.OK).json({ user: tokenUser });
 };
 const logout = async (req, res) => {
   res.cookie('token', 'logout', {
