8eb419e91f2bd5ba03d43dc5d41b7f3ea8fdd799
mnyef <vladgav004@gmail.com>
2023-05-16 23:42:39 +0300

    Hash token and Project is Complete

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,6 +7,7 @@ const {
   createTokenUser,
   sendVerificationEmail,
   sendResetPasswordEmail,
+  createHash,
 } = require('../utils');
 const crypto = require('crypto');
 const Token = require('../models/Token');
@@ -153,7 +154,7 @@ const forgotPassword = async (req, res) => {
     const tenMinutes = 1000 * 60 * 10;
     const passwordTokenExpirationDate = new Date(Date.now() + tenMinutes);
 
-    user.passwordToken = passwordToken;
+    user.passwordToken = createHash(passwordToken);
     user.passwordTokenExpirationDate = passwordTokenExpirationDate;
     await user.save();
   }
@@ -173,7 +174,7 @@ const resetPassword = async (req, res) => {
   if (user) {
     const currentDate = new Date();
     if (
-      user.passwordToken === token &&
+      user.passwordToken === createHash(token) &&
       user.passwordTokenExpirationDate > currentDate
     ) {
       user.password = password;
--- utils/index.js
+++ utils/index.js
@@ -3,6 +3,7 @@ const createTokenUser = require('./createTokenUser');
 const checkPermissions = require('./checkPermissions');
 const sendVerificationEmail = require('./sendVerificationEmail');
 const sendResetPasswordEmail = require('./sendResetPasswordEmail');
+const createHash = require('./createHash');
 
 module.exports = {
   createJWT,
@@ -12,4 +13,5 @@ module.exports = {
   checkPermissions,
   sendVerificationEmail,
   sendResetPasswordEmail,
+  createHash,
 };
